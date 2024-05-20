2e4f155e2981e9856634145d5ca5dffa1ca80929
Eun Oh <bdohhhhh@gmail.com>
2024-05-20 15:24:45 +0900

    240520 drilling first

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
 import LedgerContainer from "../components/LedgerContainer";
+import fakeData from "../data/fakedata";
+import { Expend } from "../types/d";
 
 function Home() {
+    const [expends, setExpends] = useState<Expend[]>(fakeData);
+
+    const addExpend = (newTodo: Expend) =>
+        setExpends((prev) => [...prev, newTodo]);
+
+    const deleteExpend = (expendId: string) =>
+        setExpends((prev) => prev.filter((expend) => expend.id !== expendId));
+
+    const updateExpend = (expendId: string) => setExpends((prev) => prev);
+
+    const workingToDos = expends.filter((expend) => !expend);
+
     return <LedgerContainer></LedgerContainer>;
 }
 
 export default Home;
+// prev.map((todo) =>
+//     todo.id === expendId ? { ...todo, isDone: !todo.isDone } : todo
+// )
---
+++ src/types/d.ts
@@ -0,0 +1,7 @@
+export interface Expend {
+    id: string;
+    date: string;
+    item: string;
+    amount: number;
+    description: string;
+}
