e504f5a2a21991e850da035f4956abc0f4b360e1
Eun Oh <bdohhhhh@gmail.com>
2024-05-21 17:33:47 +0900

    240521 [feat] : import alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: [
+            { find: "@", replacement: "/src" },
+            { find: "@components", replacement: "/src/components" },
+            { find: "@data", replacement: "/src/data" },
+            { find: "@pages", replacement: "/src/pages" },
+            { find: "@shared", replacement: "/src/shared" },
+        ],
+    },
+});
