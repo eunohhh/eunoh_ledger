b5ebeb2aeadf159a6065bea342abcce4dea98d7d
Eun Oh <bdohhhhh@gmail.com>
2024-05-26 20:15:19 +0900

    240526 [test] : typescript test

--- src/redux/Store.ts
+++ src/redux/Store.ts
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import ledgerReducer from "./Slice";
 
-// 와우 하나로 되네!!
 // 스토어에 리듀서 등록
 const store = configureStore({
     reducer: {
@@ -10,6 +9,7 @@ const store = configureStore({
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+// export type RootState = typeof store.getState;
 export type AppDispatch = typeof store.dispatch;
 
 export default store;
