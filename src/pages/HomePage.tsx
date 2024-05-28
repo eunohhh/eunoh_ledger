aa187a90f4a286239ec933889abe957cbcaeb63c
Eun Oh <bdohhhhh@gmail.com>
2024-05-29 05:34:00 +0900

    240529 [del] : del ledger container

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,19 @@
-import LedgerContainer from "../components/LedgerContainer";
+import Calender from "@/components/Calender";
+import Form from "@/components/Form";
+import Header from "@/components/Header";
+import List from "@/components/List";
+import Total from "@/components/Total";
 
 function Home() {
-    return <LedgerContainer />;
+    return (
+        <>
+            <Header />
+            <Form />
+            <Calender />
+            <Total />
+            <List />
+        </>
+    );
 }
 
 export default Home;
